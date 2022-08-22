c632ab3a97d8cdc6d8cbd5e02124de0c581d9bb1
Hasan Fatih Yıldırgan <fatihyildirgan@hotmail.com>
2022-08-22 13:38:41 +0300

    ters ucgen print

---
+++ tersucgen.py
@@ -0,0 +1,15 @@
+# n tane yıldızla başlayıp her satırda bir azalacak şekilde printle 1 yıldıza kadar,
+# *****
+# ****
+# ***
+# **
+# *
+
+def saydirici(yildizsayisi):
+    x = 1
+    while x <= 5:
+        print(x * "*")
+        x += 1
+
+saydirici(6)
+saydirici(5)
